ac33bbf1863aa0faef30557695f10414352c7a59
Jászberényi Gergő <gergo.jaszberenyi@gmail.com>
2022-04-08 13:04:33 +0200

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+let playVideo = document.getElementById('play-video')
+let video = document.getElementById('video')
+
+document.querySelectorAll('.content').forEach((content) => {
+  let observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.style.opacity = '1'
+        }
+      })
+    },
+    {
+      threshold: 0.3,
+      rootMargin: '0px',
+    }
+  )
+  observer.observe(content)
+})
+
+new Swiper('.swiper', {
+  slidesPerView: 2,
+  spaceBetween: 60,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
+
+playVideo.addEventListener('click', () => {
+  video.classList.remove('hidden')
+  console.log('hallo')
+})
+window.addEventListener('click', (e) => {
+  if (!video.classList.contains('hidden') && e.target != playVideo) {
+    video.classList.add('hidden')
+  }
+})
